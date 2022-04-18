c4be7861b9b0dfc5deffcecffecf2d9387c2e520
Laziz <lazizjonweb@gmail.com>
2022-04-18 16:02:58 +0500

    mob

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import "swiper/css";
+import "swiper/css/bundle";
+import "swiper/css/effect-fade";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 import "swiper/css/effect-coverflow";
--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import { Autoplay, EffectFade, Navigation } from "swiper";
 
 const Services = () => {
     return (
@@ -14,33 +17,108 @@ const Services = () => {
                         <div className="row">
                             <div className="catalogs col-lg-3">
                                 <h3>Каталог продукции</h3>
-                                <Link>
-                                    Системы управления краном
-                                </Link> <br />
-                                <Link>
-                                    Шкафы и пульты управления
-                                </Link> <br />
-                                <Link>
-                                    Кабины крановые
-                                </Link> <br />
-                                <Link>
-                                    Кресло-пульты
-                                </Link> <br />
-                                <Link>
-                                    Ремонт электрооборудования кранов
-                                </Link> <br />
-                                <Link>
-                                    Модернизация кранов
-                                </Link> <br />
-                                <Link>
-                                    Перевод кранов на радиоуправление
-                                </Link>
+
+                                <ul>
+                                    <li>
+                                        <Link>
+                                            Системы управления краном
+                                        </Link>
+                                    </li>
+                                    <li>
+                                        <Link>
+                                            Шкафы и пульты управления
+                                        </Link>
+                                    </li>
+                                    <li>
+                                        <Link>
+                                            Кабины крановые
+                                        </Link>
+                                    </li>
+                                    <li>
+                                        <Link>
+                                            Кресло-пульты
+                                        </Link>
+                                    </li>
+                                    <li>
+                                        <Link>
+                                            Ремонт электрооборудования кранов
+                                        </Link>
+                                    </li>
+                                    <li>
+                                        <Link>
+                                            Модернизация кранов
+                                        </Link>
+                                    </li>
+                                    <li>
+                                        <Link>
+                                            Перевод кранов на радиоуправление
+                                        </Link>
+                                    </li>
+                                </ul>
+
                             </div>
                             <div className="catalog-product col-lg-9">
                                 <h1>Шкафы и пульты управления</h1>
                                 <div className="section-01">
-                                    
+                                    <div className="left">
+                                        <h6>Сроки изготовления: <span>от 4 недель</span></h6>
+                                        <h6>Гарантия: <span>1 год</span></h6>
+
+                                        <a href="" className="btn myBtn">
+                                            ЗАПРОСИТЬ СТОИМОСТЬ
+                                        </a>
+
+                                        <p>Производим под заказ, учитывая индивидуальные особенности проекта</p>
+                                    </div>
+                                    <div className="right">
+                                        <Swiper
+                                            spaceBetween={30}
+                                            effect={"fade"}
+                                            navigation={true}
+                                            loop={true}
+                                            modules={[Autoplay, EffectFade, Navigation]}
+                                            autoplay={{
+                                                delay: 3000,
+                                                disableOnInteraction: false,
+                                            }}
+                                            className="mySwiper"
+                                        >
+                                            <SwiperSlide>
+                                                <img src="/image/shkaf.jpg" className='w-100' />
+                                            </SwiperSlide>
+                                            <SwiperSlide>
+                                                <img src="/image/shkaf-01.jpg" className='w-100' />
+                                            </SwiperSlide>
+                                            <SwiperSlide>
+                                                <img src="/image/shkaf-02.jpg" className='w-100' />
+                                            </SwiperSlide>
+                                            <SwiperSlide>
+                                                <img src="/image/shkaf-03.jpg" className='w-100' />
+                                            </SwiperSlide>
+                                            <SwiperSlide>
+                                                <img src="/image/shkaf-04.jpg" className='w-100' />
+                                            </SwiperSlide>
+                                            <SwiperSlide>
+                                                <img src="/image/shkaf-05.jpg" className='w-100' />
+                                            </SwiperSlide>
+                                        </Swiper>
+                                    </div>
                                 </div>
+
+                                <h1 className='mt-5'>Виды выпускаемого оборудования:</h1>
+                                <div className="section-02 d-flex flex-column">
+                                    <div className="section-02-1">
+                                        <img src="/image/shkaf-06.jpg" alt="" />
+                                        <img src="/image/shkaf-01.jpg" alt="" />
+                                    </div>
+                                    <div className="section-02-2">
+                                        <img src="/image/shkaf-07.jpg" alt="" />
+                                        <img src="/image/shkaf-08.jpg" alt="" />
+                                    </div>
+                                </div>
+
+                                <h1 className='mt-5'>Производство шкафов и пультов управления</h1>
+                                <p className='mt-3'>«БРИЗ» выпускает различные модификации продукции в соответствии с отраслевыми стандартами. Предоставляет возможность изготовления агрегатов по техническим заданиям заказчиков. Все товары сопровождаются пакетом документов и сертификатами. Купить шкафы и станции управления, узнать цены, сроки изготовления можно по телефону у специалистов или онлайн по форме обратной связи.</p>
                             </div>
                         </div>
                     </div>
